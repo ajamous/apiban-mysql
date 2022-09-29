d95fad505eb3f1bbc277522a7438e5c76c51e293
yurunhub <yura.monchak@gmail.com>
2022-09-29 16:10:47 +0000

    add CREATE SQL DDL and save ID into DB

---
+++ create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "last_fetched_id" (
+  "id" varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "apiban_banned_ips" (
+  "ip" varchar(15) NOT NULL DEFAULT '',
+  PRIMARY KEY ("ip")
+);
--- sync.js
+++ sync.js
@@ -6,6 +6,7 @@ const get = bent('https://apiban.org', 'GET', 'json', 200);
 const mysql = require('mysql2')
 
 /* validate environment */
+
 const APIBAN_API_KEY = "XXXXXXXXXXXXXXXXX"
 const APIBAN_MYSQL_SERVER = "replace_with_mysql_ip_or_host"
 const APIBAN_MYSQL_PORT = 3306
@@ -13,7 +14,7 @@ const APIBAN_MYSQL_DATABASE = "replace_with_db_name"
 const APIBAN_MYSQL_USER = "replace_with_mysql_user"
 const APIBAN_MYSQL_PASSWORD = "replace_with_mysql_password"
 
-
+var id
 
 const dbconf = {
   host: APIBAN_MYSQL_SERVER,
@@ -45,19 +46,26 @@ const db = makeDb( dbconf )
 
 
 const getBlacklist = async() => {
-  let id;
+  let sql = 'SELECT id FROM last_fetched_id LIMIT 1'
+  let result = await db.query(sql)
+  if (result[0].id != '') {
+    id = result[0].id
+  }
+  console.log("starting from ID:",id)
   let ips = [];
   do {
+    const url = `/api/${APIBAN_API_KEY}/banned${id ? ('/' + id) : ''}`;
     try {
-      const url = `/api/${APIBAN_API_KEY}/banned${id ? ('/' + id) : ''}`;
       const response = await get(url);
       //console.log("response:", response)
       ips = [...ips, ...response.ipaddress];
       id = response.ID;
-      //console.log(`fetched ${response.ipaddress.length} ips, next ID ${id}`);
+      console.log(`fetched ${response.ipaddress.length} ips, next ID ${id}`);
     } catch (err) {
       if (ips.length) return ips;
       console.error('Failed to get blacklist from apiban', err);
+      console.log("URL:"+ url + "!")
+      process.exit()
       throw err;
     }
   } while (id && id !== 'none');
@@ -75,6 +83,9 @@ async function sync() {
 		await db.query(sql,ip)
 		//console.log("checking ip",ip)
 	}
+        console.log("saving latest fetched ID into DB...:", id)
+        sql = "UPDATE last_fetched_id SET id = ?"
+        await db.query(sql, id)
 	//console.log(ips)
 	console.log("done.")
 	process.exit()
